d7d1405a4ef9e2da5ddd18ce810456d1d0536abb
Mashima <52222646+zimashima@users.noreply.github.com>
2019-11-12 15:31:30 -0500

    finished step 1 and 2, thus, finished MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,7 +10,7 @@ const Movie = (props) => {
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+        .get(`http://localhost:5000/api/movies/${id-1}`)
         .then(response => {
           setMovie(response.data);
         })
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom'
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -21,7 +22,7 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}><MovieDetails key={movie.id} movie={movie} /></Link>
       ))}
     </div>
   );
